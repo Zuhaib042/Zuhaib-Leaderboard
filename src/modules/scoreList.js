aa699bf79a39e5902af75490209852360a54ec14
Zuhaib Amjad <umar0423@gmail.com>
2022-10-12 20:57:08 +0500

    Fix linters errors

--- src/modules/scoreList.js
+++ src/modules/scoreList.js
@@ -2,7 +2,9 @@ export default class List {
   static renderList = () => {
     const ulContainer = document.getElementById('container');
     const ul = document.createElement('ul');
-    const html = `<li class="singleLi"><span>Alex :</span> <span>500</span></li><li class="singleLi"><span>David :</span> <span>600</span></li><li class="singleLi"><span>Zuhaib:</span> <span>700</span></li>`;
+    const html = `<li class='singleLi'><span>Alex :</span> <span>500</span></li>
+    <li class='singleLi'><span>David :</span> <span>600</span></li>
+    <li class='singleLi'><span>Zuhaib:</span> <span>700</span></li>`;
     ul.classList.add('scores-list', 'list-none');
     ul.insertAdjacentHTML('beforeend', html);
     ulContainer.appendChild(ul);
