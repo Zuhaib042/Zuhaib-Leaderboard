4d66cd1057627bbf79612f6753941b2410de954a
Zuhaib Amjad <umar0423@gmail.com>
2022-10-13 11:34:36 +0500

    Add final touches

--- src/index.js
+++ src/index.js
@@ -9,16 +9,8 @@ const btnRefresh = document.querySelector('.btn-scores-section');
 
 // submit the form
 form.addEventListener('submit', (e) => {
-  const successMesg = document.querySelector('.success');
   e.preventDefault();
   postApi();
-  setTimeout(() => {
-    successMesg.textContent = 'data got submitted successfully';
-    successMesg.classList.add('active');
-  }, 1500);
-  setTimeout(() => {
-    successMesg.classList.remove('active');
-  }, 4000);
 });
 
 // refresh the page
